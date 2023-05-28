9286f0b4d834657bf91e420be6da6435955bcc8d
salamijason <oattrakyaw@gmail.com>
2023-05-28 14:22:16 +0630

    Added functionality to Tokyo, London, and New York buttons up top.
    Issues left to fix as of right now:
    - changing country while in fahrenheit leads to api sending info in celsius
    in which the user can again 'change to celsius' producing wrong temp.
    
    - changing timezone based on country chosen

--- src/script.js
+++ src/script.js
@@ -26,79 +26,12 @@ month.innerHTML = `${months[now.getMonth()]}`;
 date.innerHTML = `${now.getDate()}`;
 year.innerHTML = `${now.getFullYear()}`;
 
-// search function and weather info retrieval
-function changeCountry(event) {
-  event.preventDefault();
-
-  let cityInput = document.querySelector("#city-input");
-  let currentCity = document.querySelector(`#city`);
-  currentCity.innerHTML = cityInput.value;
-
-
-  // api variables
-  let apiKey = `866a208a73eeff02182218e9441647a1`;
-  let apiUrlCity = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity.innerHTML}&units=metric&appid=${apiKey}`;
-  function changeInformation (response) {
-    let currentCity = document.querySelector(`#city`);
-    let currentCityTemp = document.querySelector(`#temp`);
-    let currentTemp = Math.round(response.data.main.temp);
-    let currentDesc = document.querySelector(`#weather`);
-    let humidity = document.querySelector(`#humidity`);
-    let wind = document.querySelector(`#wind`);
-
-    console.log(response.data);
-    currentCity.innerHTML = response.data.name;
-    currentCityTemp.innerHTML = currentTemp;
-    currentDesc.innerHTML = response.data.weather[0].description;
-    humidity.innerHTML = response.data.main.humidity;
-    wind.innerHTML = response.data.wind.speed;
-  }
-  
-  axios.get(apiUrlCity).then(changeInformation);
-}
-function setCountry(event) {
-    event.preventDefault();
-    function getLocation(position) {
-        let lat = position.coords.latitude;
-        let lon = position.coords.longitude;
-        // api variables
-        let apiKey = `866a208a73eeff02182218e9441647a1`;
-        let apiUrlCoords = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-        function changeSetLocation(response) {
-            let currentCity = document.querySelector(`#city`);
-            currentCity.innerHTML = `Current location`;
-
-            let currentCityTemp = document.querySelector(`#temp`);
-            let currentTemp = Math.round(response.data.main.temp);
-            let currentDesc = document.querySelector(`#weather`);
-            let humidity = document.querySelector(`#humidity`);
-            let wind = document.querySelector(`#wind`);
-
-            console.log(response.data);
-            currentCityTemp.innerHTML = currentTemp;    
-            currentDesc.innerHTML = response.data.weather[0].description;
-            humidity.innerHTML = response.data.main.humidity;
-            wind.innerHTML = response.data.wind.speed;
-
-        }
-        axios.get(apiUrlCoords).then(changeSetLocation);
-    }   
-    navigator.geolocation.getCurrentPosition(getLocation);
-}
-
-
-let citysearch = document.querySelector("#city-search");
-citysearch.addEventListener("submit", changeCountry);
-let locationSet = document.querySelector(`#location-set`);
-locationSet.addEventListener("click",setCountry);
-
-
 // unit change
+let celsiusButton = document.querySelector(`#celsius`);
+let fahrenheitButton = document.querySelector(`#fahrenheit`);
 function changeToFahrenheit(event) {
     //event.preventDefault(); doesn't work with for some reason
     
-    let celsiusButton = document.querySelector(`#celsius`);
-    let fahrenheitButton = document.querySelector(`#fahrenheit`);
      
     // check if it's already the unit
     if (fahrenheitButton.classList.contains(`clicked`)) {
@@ -118,9 +51,6 @@ function changeToFahrenheit(event) {
 
 function changeToCelsius(event) {
     //event.preventDefault(); doesn't work with for some reason
-    
-    let celsiusButton = document.querySelector(`#celsius`);
-    let fahrenheitButton = document.querySelector(`#fahrenheit`);
      
     // check if it's already the unit
     if (celsiusButton.classList.contains(`clicked`)) {
@@ -142,3 +72,156 @@ let changeToCelsiusButton = document.querySelector(`#celsius`);
 let changeToFahrenheitButton = document.querySelector(`#fahrenheit`);
 changeToCelsiusButton.addEventListener(`click`,changeToCelsius);
 changeToFahrenheitButton.addEventListener(`click`,changeToFahrenheit);
+
+// search function and weather info retrieval
+let apiKey = `866a208a73eeff02182218e9441647a1`;
+let currentCity = document.querySelector(`#city`);
+let currentCityTemp = document.querySelector(`#temp`);
+let currentDesc = document.querySelector(`#weather`);
+let humidity = document.querySelector(`#humidity`);
+let wind = document.querySelector(`#wind`);
+let unit = 'metric'
+if (celsiusButton.classList.contains(`clicked`)) {
+    unit = 'metric';
+} else {
+    unit = 'imperial';
+}
+
+function changeCountry(event) {
+  event.preventDefault();
+
+  let cityInput = document.querySelector("#city-input");
+  currentCity.innerHTML = cityInput.value;
+
+
+  // api variables
+  let apiUrlCity = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity.innerHTML}&units=${unit}&appid=${apiKey}`;
+  function changeInformation (response) {
+    let currentTemp = Math.round(response.data.main.temp);
+    
+
+    console.log(response.data);
+    currentCity.innerHTML = response.data.name;
+    currentCityTemp.innerHTML = currentTemp;
+    currentDesc.innerHTML = response.data.weather[0].description;
+    humidity.innerHTML = response.data.main.humidity;
+    wind.innerHTML = response.data.wind.speed;
+  }
+  
+  axios.get(apiUrlCity).then(changeInformation);
+}
+function setCountry(event) {
+    event.preventDefault();
+    function getLocation(position) {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+        // api variables
+        let apiKey = `866a208a73eeff02182218e9441647a1`;
+        let apiUrlCoords = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${unit}&appid=${apiKey}`;
+        function changeSetLocation(response) {
+            currentCity.innerHTML = `Current location`;
+
+            let currentTemp = Math.round(response.data.main.temp);
+
+            console.log(response.data);
+            currentCityTemp.innerHTML = currentTemp;    
+            currentDesc.innerHTML = response.data.weather[0].description;
+            humidity.innerHTML = response.data.main.humidity;
+            wind.innerHTML = response.data.wind.speed;
+
+        }
+        axios.get(apiUrlCoords).then(changeSetLocation);
+    }   
+    navigator.geolocation.getCurrentPosition(getLocation);
+    tokyoButton.classList.remove(`clicked`);
+    londonButton.classList.remove(`clicked`);
+    newyorkButton.classList.remove('clicked');
+}
+
+
+let citysearch = document.querySelector("#city-search");
+citysearch.addEventListener("submit", changeCountry);
+let locationSet = document.querySelector(`#location-set`);
+locationSet.addEventListener("click",setCountry);
+
+// location buttons 
+function setTokyo(event) {
+  event.preventDefault();
+  currentCity.innerHTML = `Tokyo`;
+
+  // api variables
+  let apiUrlCity = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity.innerHTML}&units=${unit}&appid=${apiKey}`;
+  function changeInformation (response) {
+    let currentTemp = Math.round(response.data.main.temp);
+    console.log(response.data);
+    currentCity.innerHTML = response.data.name;
+    currentCityTemp.innerHTML = currentTemp;
+    currentDesc.innerHTML = response.data.weather[0].description;
+    humidity.innerHTML = response.data.main.humidity;
+    wind.innerHTML = response.data.wind.speed;
+  }
+  
+  axios.get(apiUrlCity).then(changeInformation);
+  tokyoButton.classList.add(`clicked`);
+  londonButton.classList.remove(`clicked`);
+  newyorkButton.classList.remove('clicked');
+}
+
+function setLondon(event) {
+  event.preventDefault();
+
+  
+  currentCity.innerHTML = `London`;
+
+
+  // api variables
+  let apiUrlCity = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity.innerHTML}&units=${unit}&appid=${apiKey}`;
+  function changeInformation (response) {
+    let currentTemp = Math.round(response.data.main.temp);
+    console.log(response.data);
+    currentCity.innerHTML = response.data.name;
+    currentCityTemp.innerHTML = currentTemp;
+    currentDesc.innerHTML = response.data.weather[0].description;
+    humidity.innerHTML = response.data.main.humidity;
+    wind.innerHTML = response.data.wind.speed;
+  }
+  axios.get(apiUrlCity).then(changeInformation);
+  tokyoButton.classList.remove(`clicked`);
+  londonButton.classList.add(`clicked`);
+  newyorkButton.classList.remove('clicked');
+}
+
+function setNewYork(event) {
+  event.preventDefault();
+
+  
+  currentCity.innerHTML = `New York`;
+
+
+  // api variables
+  let apiUrlCity = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity.innerHTML}&units=${unit}&appid=${apiKey}`;
+  function changeInformation (response) {
+    let currentTemp = Math.round(response.data.main.temp);
+    console.log(response.data);
+    currentCity.innerHTML = response.data.name;
+    currentCityTemp.innerHTML = currentTemp;
+    currentDesc.innerHTML = response.data.weather[0].description;
+    humidity.innerHTML = response.data.main.humidity;
+    wind.innerHTML = response.data.wind.speed;
+  }
+  
+  axios.get(apiUrlCity).then(changeInformation);
+  tokyoButton.classList.remove(`clicked`);
+  londonButton.classList.remove(`clicked`);
+  newyorkButton.classList.add('clicked');
+}
+
+let tokyoButton = document.querySelector(`#tokyo-button`);
+let londonButton = document.querySelector(`#london-button`);
+let newyorkButton = document.querySelector(`#newyork-button`);
+tokyoButton.addEventListener(`click`, setTokyo);
+londonButton.addEventListener(`click`, setLondon);
+newyorkButton.addEventListener(`click`, setNewYork);
+
+
+
